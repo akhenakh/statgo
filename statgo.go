744b40953e75a421682985156fef1b8039cba6c8
Fabrice Aneche <akh@nobugware.com>
2015-07-31 16:32:24 -0400

    get rid of cpu_percent ptr

--- statgo.go
+++ statgo.go
@@ -17,7 +17,6 @@ var lock sync.Mutex
 
 // Stat handle to access libstatgrab
 type Stat struct {
-	cpu_percent *C.sg_cpu_percents
 }
 
 // HostInfo contains informations related to the system
@@ -104,23 +103,24 @@ func (s *Stat) HostInfo() *HostInfo {
 func (s *Stat) CPUStats() *CPUStats {
 	lock.Lock()
 	defer lock.Unlock()
+
 	// Throw away the first reading as thats averaged over the machines uptime
 	C.sg_snapshot()
 	C.sg_get_cpu_stats_diff(nil)
 
-	s.cpu_percent = C.sg_get_cpu_percents_of(C.sg_last_diff_cpu_percent, nil)
+	cpu_percent := C.sg_get_cpu_percents_of(C.sg_last_diff_cpu_percent, nil)
 
 	C.sg_snapshot()
 
 	load_stat := C.sg_get_load_stats(nil)
 
 	cpu := &CPUStats{
-		User:      float64(C.double(s.cpu_percent.user)),
-		Kernel:    float64(C.double(s.cpu_percent.kernel)),
-		Idle:      float64(C.double(s.cpu_percent.idle)),
-		IOWait:    float64(C.double(s.cpu_percent.iowait)),
-		Swap:      float64(C.double(s.cpu_percent.swap)),
-		Nice:      float64(C.double(s.cpu_percent.nice)),
+		User:      float64(C.double(cpu_percent.user)),
+		Kernel:    float64(C.double(cpu_percent.kernel)),
+		Idle:      float64(C.double(cpu_percent.idle)),
+		IOWait:    float64(C.double(cpu_percent.iowait)),
+		Swap:      float64(C.double(cpu_percent.swap)),
+		Nice:      float64(C.double(cpu_percent.nice)),
 		LoadMin1:  float64(C.double(load_stat.min1)),
 		LoadMin5:  float64(C.double(load_stat.min5)),
 		LoadMin15: float64(C.double(load_stat.min15)),
